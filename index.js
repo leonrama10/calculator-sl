54c8dd3b615aa127d7a1ae1b951913fe8cc7a414
leonrama10 <151144347+leonrama10@users.noreply.github.com>
2024-10-22 17:16:59 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,30 @@
+const display = document.querySelector(".display");
+const buttons = document.querySelectorAll("button");
+const specialChars = ["%", "*", "/", "-", "+", "="];
+let output = "";
+
+const calculate = (btnValue) => {
+  display.focus();
+
+  if (btnValue === "=" && output !== "") {
+    output = eval(output.replace("%", "/100"));
+  } else if (btnValue === "AC") {
+    output = "";
+  } else if (btnValue === "DEL") {
+    output = output.toString().slice(0, -1);
+  } else {
+
+    const lastChar = output[output.length - 1];
+    if (specialChars.includes(btnValue) && specialChars.includes(lastChar)) {
+      return; 
+    }
+    if (output === "" && specialChars.includes(btnValue)) return; 
+    
+    output += btnValue;
+  }
+  display.value = output;
+};
+
+buttons.forEach((button) => {
+  button.addEventListener("click", (e) => calculate(e.target.dataset.value));
+});
